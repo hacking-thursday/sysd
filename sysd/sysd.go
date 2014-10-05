19b315a0f4ce24700f5476c01c413ab5b1a0ce7c
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2014-10-06 00:48:35 +0800

    使用 go fmt 校正語法排版與縮排

--- sysd/sysd.go
+++ sysd/sysd.go
@@ -1,56 +1,56 @@
 package main
 
 import (
-    "log"
-    "builtins"
-    "daemon"
-    "github.com/docker/docker/engine"
-    "os/exec"
+	"builtins"
+	"daemon"
+	"github.com/docker/docker/engine"
+	"log"
+	"os/exec"
 )
 
 func main() {
-    host := "127.0.0.1:4000"
+	host := "127.0.0.1:4000"
 
-    eng := engine.New()
-    if err := builtins.Register(eng); err != nil {
-            log.Fatal(err)
-    }
+	eng := engine.New()
+	if err := builtins.Register(eng); err != nil {
+		log.Fatal(err)
+	}
 
-    // 註冊自定義延伸指令
-    eng.Register("info2", func(job *engine.Job) engine.Status {
-            v := &engine.Env{}
+	// 註冊自定義延伸指令
+	eng.Register("info2", func(job *engine.Job) engine.Status {
+		v := &engine.Env{}
 
-            out, err := exec.Command("uname","-a").Output()
-	    if( err == nil ){
-		    v.Set( "uname", string(out) )
-	    }
+		out, err := exec.Command("uname", "-a").Output()
+		if err == nil {
+			v.Set("uname", string(out))
+		}
 
-            if _, err := v.WriteTo(job.Stdout); err != nil {
-                    return job.Error(err)
-            }
-            return engine.StatusOK
-    })
+		if _, err := v.WriteTo(job.Stdout); err != nil {
+			return job.Error(err)
+		}
+		return engine.StatusOK
+	})
 
-    go func() {
-            daemonCfg := &daemon.Config{}
-            daemonCfg.InstallFlags()
-            daemonCfg.Pidfile = "/tmp/sysd.pid"
+	go func() {
+		daemonCfg := &daemon.Config{}
+		daemonCfg.InstallFlags()
+		daemonCfg.Pidfile = "/tmp/sysd.pid"
 
-            d, err := daemon.NewDaemon(daemonCfg, eng)
-            if err != nil {
-                    log.Fatal(err)
-            }
-            if err := d.Install(eng); err != nil {
-                    log.Fatal(err)
-            }
+		d, err := daemon.NewDaemon(daemonCfg, eng)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := d.Install(eng); err != nil {
+			log.Fatal(err)
+		}
 
-            if err := eng.Job("acceptconnections").Run(); err != nil {
-                    log.Fatal(err)
-            }
-    }()
+		if err := eng.Job("acceptconnections").Run(); err != nil {
+			log.Fatal(err)
+		}
+	}()
 
-    job := eng.Job("serveapi", "tcp://"+host)
-    if err := job.Run(); err != nil {
-	    log.Fatal(err)
-    }
+	job := eng.Job("serveapi", "tcp://"+host)
+	if err := job.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
