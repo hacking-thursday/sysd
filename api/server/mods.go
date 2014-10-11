b14053113470eaeeae159cd2398be18e04dc689a
tsaikd <tsaikd@gmail.com>
2014-10-11 22:38:44 +0800

    add mods/ifconfig

--- api/server/mods.go
+++ api/server/mods.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	_ "github.com/hacking-thursday/sysd/mods/ifconfig"
 	_ "github.com/hacking-thursday/sysd/mods/memstats"
 	_ "github.com/hacking-thursday/sysd/mods/ping"
 )
