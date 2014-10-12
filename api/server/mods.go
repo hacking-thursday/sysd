448b3d808109791c7ad84222d8e2a21a8125730d
tsaikd <tsaikd@gmail.com>
2014-10-12 23:11:05 +0800

    add mods/apilist, list all registed api

--- api/server/mods.go
+++ api/server/mods.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	_ "github.com/hacking-thursday/sysd/mods/apilist"
 	_ "github.com/hacking-thursday/sysd/mods/ifconfig"
 	_ "github.com/hacking-thursday/sysd/mods/memstats"
 	_ "github.com/hacking-thursday/sysd/mods/ping"
