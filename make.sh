f7ed9f269aa2de591c01e7356e19cd7f97af4478
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2014-10-13 00:44:00 +0800

    將 make.sh 中的路徑變數都括起來，以避免 windows 平台的路徑空格

--- make.sh
+++ make.sh
@@ -21,7 +21,7 @@ H4DIR="$GOPATH0/src/github.com/hacking-thursday"
 SYSDDIR="$H4DIR/sysd"
 
 function replace_sysd_dir(){
-    pushd $H4DIR > /dev/null
+    pushd "$H4DIR" > /dev/null
         if [ -d "./sysd" ]; then
             rm -rf "./sysd"
             ln -s ../../../../ ./sysd
@@ -29,24 +29,24 @@ function replace_sysd_dir(){
     popd > /dev/null
 }
 
-if [ ! -d $TMPDIR ]; then mkdir -p $TMPDIR ; fi
-if [ ! -d $SYSDDIR ]; then mkdir -p $SYSDDIR ; fi
+if [ ! -d "$TMPDIR" ]; then mkdir -p "$TMPDIR" ; fi
+if [ ! -d "$SYSDDIR" ]; then mkdir -p "$SYSDDIR" ; fi
 
-if [ -L $SYSDDIR ];then 
+if [ -L "$SYSDDIR" ];then 
     echo -n -e "[Symbolic link]\t"; 
-    MESSAGE=" => $(readlink -f $SYSDDIR)";
+    MESSAGE=" => $( cd $SYSDDIR && pwd -P )";
 else
     echo -n -e "[Mirror copy]\t"; 
 fi
 echo "$SYSDDIR $MESSAGE"
 
-if [ -d $SYSDDIR -a ! -L $SYSDDIR ] ;then
-    cp -r $ROOT/* $SYSDDIR/
-    pushd $SYSDDIR
+if [ -d "$SYSDDIR" -a ! -L "$SYSDDIR" ] ;then
+    cp -r "$ROOT"/* "$SYSDDIR/"
+    pushd "$SYSDDIR"
         TARGET_PATH="${GOPATH0}/src/github.com/docker/libcontainer/cgroups/systemd/apply_systemd.go"
-        grep -e 'theConn\.StartTransientUnit.*, nil);' $TARGET_PATH
-        if [ $? -eq 1 -a -f $TARGET_PATH ]; then
-            cp -v $ROOT/misc/apply_systemd.go $TARGET_PATH
+        grep -e 'theConn\.StartTransientUnit.*, nil);' "$TARGET_PATH"
+        if [ $? -eq 1 -a -f "$TARGET_PATH" ]; then
+            cp -v "$ROOT/misc/apply_systemd.go" "$TARGET_PATH"
         fi
         go get -v -t -tags "$BuildTags" ./sysd
         if [ $? -eq 0 ];then
@@ -55,13 +55,13 @@ if [ -d $SYSDDIR -a ! -L $SYSDDIR ] ;then
     popd
 fi
 
-if [ -L $SYSDDIR ];then
+if [ -L "$SYSDDIR" ];then
     # build first
-    pushd $SYSDDIR/sysd
+    pushd "$SYSDDIR/sysd"
         go build -v -tags "$BuildTags"
         if [ $? -eq 0 ];then
             # test later
-            pushd $SYSDDIR
+            pushd "$SYSDDIR"
                 go test -v -tags "$BuildTags" ./...
             popd
         fi
