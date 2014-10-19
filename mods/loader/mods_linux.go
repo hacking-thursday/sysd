1904f9048a1851ad059a202634f4f634734fc966
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2014-10-20 01:16:29 +0800

    新增 meminfo 模組

--- mods/loader/mods_linux.go
+++ mods/loader/mods_linux.go
@@ -3,4 +3,5 @@ package loader
 import (
 	_ "github.com/hacking-thursday/sysd/mods/sysinfo"
 	_ "github.com/hacking-thursday/sysd/mods/cpuinfo"
+	_ "github.com/hacking-thursday/sysd/mods/meminfo"
 )
