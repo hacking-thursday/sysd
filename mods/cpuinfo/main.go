402dee0e98ee563d4cbe4b7f0a85f9abb7ce3ce6
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2014-10-20 01:06:43 +0800

    新增 cpuinfo 模組

---
+++ mods/cpuinfo/main.go
@@ -0,0 +1,57 @@
+// +build linux
+
+package cpuinfo
+
+import (
+	"bufio"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/docker/docker/pkg/log"
+	"github.com/docker/docker/pkg/version"
+	"github.com/hacking-thursday/sysd/mods"
+)
+
+func init() {
+	log.Debugf("Initializing module...")
+	mods.Register("GET", "/cpuinfo", handler)
+}
+
+func handler(eng_ifce interface{}, version version.Version, w http.ResponseWriter, r *http.Request, vars map[string]string) (err error) {
+	target_path := "/proc/cpuinfo"
+	result := []map[string]string{}
+	var out []byte
+
+	f, err := os.Open(target_path)
+	scanner := bufio.NewScanner(f)
+	i := 0
+	row := map[string]string{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line == "" {
+			result = append(result, row)
+                        row = map[string]string{}
+
+			i += 1
+		}
+
+		ary := strings.Split(line, ":")
+		if len(ary) == 2 {
+			key := strings.TrimSpace(ary[0])
+			val := strings.TrimSpace(ary[1])
+			row[key] = val
+		}
+	}
+
+	if out, err = mods.Marshal(r, result); err != nil {
+		mods.HttpError(w, err)
+		return
+	}
+
+	if _, err = w.Write(out); err != nil {
+		mods.HttpError(w, err)
+		return
+	}
+	return
+}
--- mods/loader/mods_linux.go
+++ mods/loader/mods_linux.go
@@ -2,4 +2,5 @@ package loader
 
 import (
 	_ "github.com/hacking-thursday/sysd/mods/sysinfo"
+	_ "github.com/hacking-thursday/sysd/mods/cpuinfo"
 )
