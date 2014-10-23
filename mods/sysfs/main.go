0f11666e067c176439434294d2fe1513efbcb64d
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2014-10-23 14:05:38 +0800

    新增列舉 /sys/dev/ 目錄

--- mods/sysfs/main.go
+++ mods/sysfs/main.go
@@ -58,6 +58,38 @@ func handler(eng_ifce interface{}, version version.Version, w http.ResponseWrite
 		}
 	}
 
+        subdir := "dev"
+	target_path = "/sys/" + subdir
+	result[subdir] = TreeNode{}
+
+	d_pathes, err = ioutil.ReadDir(target_path)
+	if err == nil {
+		for i := 0; i < len(d_pathes); i++ {
+			target_path2 := path.Join(target_path, d_pathes[i].Name())
+			d_pathes2, err2 := ioutil.ReadDir(target_path2)
+
+			if err2 == nil {
+				for j := 0; j < len(d_pathes2); j++ {
+					target_path3 := path.Join(target_path2, d_pathes2[j].Name())
+					target_link3, _ := os.Readlink(target_path3)
+					target_link3, _ = filepath.EvalSymlinks(target_path3)
+					row := [2]string{target_path3, target_link3}
+
+					key1 := d_pathes[i].Name()
+					key2 := d_pathes2[j].Name()
+
+					if _, ok := result[subdir].(TreeNode)[key1]; !ok {
+						result[subdir].(TreeNode)[key1] = TreeNode{}
+					}
+
+					if _, ok := result[subdir].(TreeNode)[key1].(TreeNode)[key2]; !ok {
+						result[subdir].(TreeNode)[key1].(TreeNode)[key2] = row
+					}
+				}
+			}
+		}
+	}
+
 	if out, err = mods.Marshal(r, result); err != nil {
 		mods.HttpError(w, err)
 		return
