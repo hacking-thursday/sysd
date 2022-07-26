d64dc8dead52da37643fb1f6e445c08d146dfa8e
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2022-07-27 03:46:03 +0800

    [WIP] Update and simplify make scripts
    Signed-off-by: Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>

--- Makefile
+++ Makefile
@@ -1,13 +1,10 @@
 PWD=$(shell pwd)
 
 build:
-	./scripts/make.sh || true
+	./scripts/make.sh
 
 run:
 	DEBUG=1 SYSD_UI_DIR="$$(pwd)/mods/ui/files" sysd/sysd
-
-test:
-	env
 
 doc:
 	@echo "" > docs/source/modules.rst
@@ -15,9 +12,8 @@ doc:
 	cd docs; make html
 
 clean:
-	rm -rf .gopath || true
-	rm -rf .tmp || true
-	rm -rf sysd/sysd || true
+	[ -e .gopath ] && rm -rf .gopath || true
+	[ -e sysd/sysd ] && rm -rf sysd/sysd || true
 
 install:
 	install -D --mode=0644 sysd/sysd $(DESTDIR)/usr/sbin/sysd
--- scripts/make.sh
+++ scripts/make.sh
@@ -2,35 +2,10 @@
 
 set -e
 
-ROOT="$( cd $( dirname $0 ) && pwd -P )/.."
-VENDOR="$ROOT/vendor"
+(
+	cd $(dirname $0)/../sysd || exit
 
-GOPATH0="$ROOT/.gopath"
-GOPATH="$VENDOR:$GOPATH0:$GOPATH"
-GOPATH="$( echo $GOPATH | sed -e 's/:$//g' )" # drop tralling comma
-echo "GOPATH: $GOPATH"
-export GOPATH
-
-export TMPDIR="$ROOT/.tmp"
-export CGO_ENABLED="0" # 有效減少 dependencies
-export BuildTags="$BuildTags exclude_graphdriver_devicemapper exclude_graphdriver_aufs exclude_graphdriver_btrfs" # 有效減少 dependencies
-
-SYSDDIR="$GOPATH0/src/github.com/hacking-thursday/sysd"
-
-function sync_sysd_dir(){
-    if [ -d "$SYSDDIR" -o -L "$SYSDDIR" ] ;then
-        rm -rf "$SYSDDIR"
-    fi
-
-    echo "Syncing files: $ROOT => $SYSDDIR "
-    mkdir -p "$SYSDDIR"
-    cp -r "$ROOT"/* "$SYSDDIR/"
-}
-
-if [ ! -d "$TMPDIR" ]; then mkdir -p "$TMPDIR" ; fi
-
-sync_sysd_dir
-
-pushd "$ROOT/sysd"
-    go build -v -tags "$BuildTags"
-popd
+	export GOPATH="$(pwd)/.gopath:$(pwd)/vendor"
+	go get
+	go build -v
+)
