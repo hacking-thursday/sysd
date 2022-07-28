42e8ed93da4d16df052f6c7071fdc0c3172af7fa
Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>
2022-07-28 16:22:16 +0800

    [WIP] Add a sample GUI prototype based on Tauri
    Signed-off-by: Chun-Yu Lee (Mat) <matlinuxer2@gmail.com>

---
+++ samples/gui/README.md
@@ -0,0 +1,12 @@
+A Sysd GUI based on tauri.
+
+## Development
+
+!!!note
+    This folder could take around 3~4 GB storage space.
+
+```
+$ cd gui
+$ npm install
+$ npm run tauri dev
+```
---
+++ samples/gui/src-tauri/src/main.rs
@@ -0,0 +1,22 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+use tauri::{CustomMenuItem, SystemTray, SystemTrayMenu, SystemTrayMenuItem};
+
+fn main() {
+  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+  let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+  let tray_menu = SystemTrayMenu::new()
+      .add_item(quit)
+      .add_native_item(SystemTrayMenuItem::Separator)
+      .add_item(hide);
+  let system_tray = SystemTray::new()
+      .with_menu(tray_menu);
+
+  tauri::Builder::default()
+    .system_tray(system_tray)
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
